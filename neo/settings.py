172699271a25a0475eecb745ed1d0ced818d4e4b
AdrianEnriquez4505 <adrian.enriquez4505@gmail.com>
2023-11-10 09:36:10 -0500

    allow inserrtion

--- neo/settings.py
+++ neo/settings.py
@@ -123,12 +123,12 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    )
+    #'DEFAULT_PERMISSION_CLASSES': (
+    #    'rest_framework.permissions.IsAuthenticated',
+    #),
+    #'DEFAULT_AUTHENTICATION_CLASSES': (
+    #    'rest_framework.authentication.TokenAuthentication',
+    #)
 }
 
 AUTHENTICATION_CLASSES = (
